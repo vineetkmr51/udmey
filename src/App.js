ab0cb3470a04bbb7218164103fac4347bd83b031
viiniitphygitix <viiniit.aroraa@phygitix.ai>
2024-01-06 00:27:44 +0530

    npm install react-bootstrap bootstrap
    DD

--- src/App.js
+++ src/App.js
@@ -1,52 +1,32 @@
+import React, { useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 import Header from './components/Header.js';
-import CoreConcept from './components/CoreConcept.js';
-import { Core_Concept } from './data.js';
-import TabButton from './components/TabButton.js';
-// import EXAMPLES from './data-with-examples.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import MainPage from './pages/landing';
+import Error from "./pages/error/index.js";
 
 function App() {
-  let tabContent = 'Please click a button';
-  // const [selectedTopic, setSelectedTopic] = useState('components');
-  function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
-    tabContent = selectedButton;
-    // setSelectedTopic(selectedButton)
-    console.log(selectedButton);
-  }
   return (
     <div>
       <Header />
-      <main>
-        <h2>Time to get started!</h2>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            <CoreConcept title={Core_Concept[0].title} description={Core_Concept[0].description} image={Core_Concept[0].image} />
-            <CoreConcept {...Core_Concept[1]} />
-            <CoreConcept {...Core_Concept[2]} />
-            <CoreConcept {...Core_Concept[3]} />
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton onSelect={() => handleSelect('components')} >Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')} >JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')} >Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
-          </menu>
-          {tabContent}
-          {/* <div id ="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>{EXAMPLES[selectedTopic].code}</pre>
-            </div> */}
-        </section>
-      </main>
+     <Router>
+      <div>
+       
+        <Routes>
+          <Route exact path="/" Component={MainPage}/>
 
+          
+          <Route exact path="*" Component={Error}/>
+        </Routes>
+      </div>
+    </Router>
+     
+   
 
 
     </div>
---
+++ src/pages/landing/index.js
@@ -0,0 +1,54 @@
+
+import CoreConcept from '../../components/CoreConcept.js';
+import { Core_Concept } from '../../data.js';
+import TabButton from '../../components/TabButton.js';
+// import EXAMPLES from './data-with-examples.js';
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function Landing() {
+  let tabContent = 'Please click a button';
+  // const [selectedTopic, setSelectedTopic] = useState('components');
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    tabContent = selectedButton;
+    // setSelectedTopic(selectedButton)
+    console.log(selectedButton);
+  }
+  return (
+    <div>
+      
+      <main>
+        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title={Core_Concept[0].title} description={Core_Concept[0].description} image={Core_Concept[0].image} />
+            <CoreConcept {...Core_Concept[1]} />
+            <CoreConcept {...Core_Concept[2]} />
+            <CoreConcept {...Core_Concept[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')} >Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')} >JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')} >Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')} >State</TabButton>
+          </menu>
+          {tabContent}
+          {/* <div id ="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>{EXAMPLES[selectedTopic].code}</pre>
+            </div> */}
+        </section>
+      </main>
+
+
+
+    </div>
+  );
+}
+
+export default Landing;
