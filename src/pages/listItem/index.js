ff75b618242edde272010cbd297de59736898e97
Viiniit Aroaa <vineet@radiansys.com>
2024-01-15 13:21:34 +0530

    api url change

--- src/pages/listItem/index.js
+++ src/pages/listItem/index.js
@@ -1,31 +1,30 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 function List() {
-
-  const[test, setTest] = useState([{name:"default"}])
+  const [test, setTest] = useState([
+    { title: "default", id: "testId", completed: "cs" },
+  ]);
   useEffect(() => {
-    apifunc()
-  }, [])
+    apifunc();
+  }, []);
 
-  async function apifunc(){
-      let apiData = await fetch('https://jsonplaceholder.typicode.com/users')
-      let apijson = await apiData.json()
-      setTest(apijson)
+  async function apifunc() {
+    let apiData = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    let apijson = await apiData.json();
+    setTest(apijson);
   }
   return (
-    <div>
-      
-      {
-        test.map((item) => {
-          return(
-            <div>
-              {item.name}
+    <div className="container">
+      {test.map((item) => {
+        return (
+          <div className="card mb-4">
+            <div className="card-body">
+              <p className="card-title">{item.id}</p>
+              <h5 className="card-title">{item.title}</h5>
+              <p className="card-text">{item.completed}</p>
             </div>
-          )
-        })
-      }
-
-
-
+          </div>
+        );
+      })}
     </div>
   );
 }
